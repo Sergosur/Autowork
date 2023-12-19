f0949e1faf90a5e19bb4ab5a771a3c19c4043ed7
Sergey <shmatkovsa@gmail.com>
2023-12-19 21:34:31 +0300

    fail JS обновлен

--- new.js
+++ new.js
@@ -1,9 +1,16 @@
 console.log('Скрипт работает');
-console.log(weight.value);  /* Комментарий */
-console.log(document.forms.my_body.mass.value);
+//console.log(weight.value);  /* Комментарий */
+//console.log(document.forms.my_body.mass.value);
 function calc_age(year_of_birth) {
     let dt = new Date();
     var result = dt.getFullYear() - year_of_birth;
     age.innerHTML = result;
 	return result;
 }
+var year = 2013
+var words = {  // пары 'ключ': 'значение'
+	'dictionary': 'словарь',
+	'task': 'задание'
+};
+console.log(words['task']);
+console.log(words.task);
