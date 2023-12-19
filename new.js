17eee4c2ded9e66bd420054fae4eac88630de6aa
Sergey <shmatkovsa@gmail.com>
2023-12-19 20:55:12 +0300

    year

--- new.js
+++ new.js
@@ -1,2 +1,9 @@
 console.log('Скрипт работает');
-lalalal
+console.log(weight.value);  /* Комментарий */
+console.log(document.forms.my_body.mass.value);
+function calc_age(year_of_birth) {
+    let dt = new Date();
+    var result = dt.getFullYear() - year_of_birth;
+    age.innerHTML = result;
+	return result;
+}
